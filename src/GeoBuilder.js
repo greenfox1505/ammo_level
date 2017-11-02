50d5096b433d2a17daaebce9a3ca274d15f65cfc
Green Fox <GreenFox@zombiejerky.net>
2017-11-01 20:40:22 -0500

    Replaced Oimojs with Cannon!

--- src/GeoBuilder.js
+++ src/GeoBuilder.js
@@ -1,21 +1,24 @@
 var THREE = require("three")
+var CANNON = require("cannon")
 
-module.exports = function(geoArgs){//todo verify input
-    if(geoArgs[0] == "cube"){
-        var physics = {
-            type:'box', 
-            size: [geoArgs[1],geoArgs[2],geoArgs[3]]
+module.exports = function (geoArgs) {//todo verify input
+    var output;
+    if (geoArgs[0] == "cube") {
+        output = {
+            render: new THREE.BoxBufferGeometry(geoArgs[1], geoArgs[2], geoArgs[3]),
+            physics: new CANNON.Box(new CANNON.Vec3(geoArgs[1] / 2, geoArgs[2] / 2, geoArgs[3] / 2))
         }
-        return [new THREE.BoxBufferGeometry( geoArgs[1],geoArgs[2],geoArgs[3] ),physics]
     }
-    else if(geoArgs[0] == "sphere"){
-        var physics = {
-            type:'sphere', 
-            size: [geoArgs[1],geoArgs[2],geoArgs[3]]
+    else if (geoArgs[0] == "sphere") {
+        output = {
+            render: new THREE.SphereBufferGeometry(geoArgs[1], geoArgs[2], geoArgs[3]),
+            physics: new CANNON.Sphere(geoArgs[1])
         }
-        return [new THREE.SphereBufferGeometry( geoArgs[1],geoArgs[2],geoArgs[3] ),physics]
+        console
     }
-    else{
+    else {
         throw "GEOMETRY NOT SUPPORTED!"
     }
+    console.log(output)
+    return output;
 }    
--- src/levelLoader.js
+++ src/levelLoader.js
@@ -1,5 +1,5 @@
 var THREE = require("three")
-var OIMO = require("oimo")
+var CANNON = require("cannon")
 
 var GeoBuilder = require("./GeoBuilder.js")
 var MatBuilder = require("./MatBuilder.js")
@@ -8,29 +8,23 @@ module.exports = function (interfaces, rawLevel) {
     var scene = new THREE.Scene();
 
     var player = {};
-    if(rawLevel.player){
-        player =JSON.parse(JSON.stringify(rawLevel.player))
+    if (rawLevel.player) {
+        player = JSON.parse(JSON.stringify(rawLevel.player))
     }
     var renderer = interfaces.renderer;
     if (rawLevel.world.background) { renderer = renderer.setClearColor(rawLevel.world.background, 1); }
 
-    var world = interfaces.world = new OIMO.World({
-        timestep: 1 / 60, //probably needs to become dynamic for framerate reasons
-        iterations: 8,
-        broadphase: 2, // 1 brute force, 2 sweep and prune, 3 volume tree
-        worldscale: 1, // scale full world 
-        random: true,  // randomize sample
-        info: false,   // calculate statistic or not
-        gravity: rawLevel.world.grav
-    });
+
+    var world = new CANNON.World();
+    world.gravity.set(rawLevel.world.grav[0], rawLevel.world.grav[1], rawLevel.world.grav[2]); // m/s²
 
     var level = { //fully loaded level
         mats: {},
         geos: {},
         objs: {},
-        renderWorld:scene,
+        renderWorld: scene,
         phyWorld: world,
-        player:player,
+        player: player,
     }
 
     for (var i in rawLevel.geos) {
@@ -43,37 +37,34 @@ module.exports = function (interfaces, rawLevel) {
         try {
             var geo = level.geos[rawLevel.objs[i][0]]
             var mat = level.mats[rawLevel.objs[i][1]]
-            //todo physics stuffs!
-            var obj = level.objs[i] = new THREE.Mesh(geo[0], mat[0]);
-            if (rawLevel.objs[i][2]) {//positions
-                obj.position.x = rawLevel.objs[i][2][0]
-                obj.position.y = rawLevel.objs[i][2][1]
-                obj.position.z = rawLevel.objs[i][2][2]
-            }
-            if (rawLevel.objs[i][3]) {//rotations,radians
-                obj.rotation.x = rawLevel.objs[i][3][0] * Math.PI * 2
-                obj.rotation.y = rawLevel.objs[i][3][1] * Math.PI * 2 * 360
-                obj.rotation.z = rawLevel.objs[i][3][2] * Math.PI * 2
-            }
-            obj.phys = world.add({
-                type: geo[1].type, // type of shape : sphere, box, cylinder 
-                size: geo[1].size, // size of shape
-                pos: rawLevel.objs[i][2],
-                rot: [
-                    rawLevel.objs[i][3][0] * 360,
-                    rawLevel.objs[i][3][1] * 360,
-                    rawLevel.objs[i][3][2] * 360
-                ], // start rotation in degree
-                move: mat[1].move, // dynamic or statique
-                density: mat[1].density,
-                friction: mat[1].friction,
-                restitution: mat[1].restitution,
-                belongsTo: 1, // The bits of the collision groups to which the shape belongs.
-                collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.
-            })
+            var obj = level.objs[i] = new THREE.Mesh(geo.render, mat.render);
+            // if (rawLevel.objs[i][2]) {//positions
+            //     obj.position.x = rawLevel.objs[i][2][0]
+            //     obj.position.y = rawLevel.objs[i][2][1]
+            //     obj.position.z = rawLevel.objs[i][2][2]
+            // }
+            // if (rawLevel.objs[i][3]) {//rotations,radians
+            //     obj.rotation.x = rawLevel.objs[i][3][0] * Math.PI * 2
+            //     obj.rotation.y = rawLevel.objs[i][3][1] * Math.PI * 2 * 360
+            //     obj.rotation.z = rawLevel.objs[i][3][2] * Math.PI * 2
+            // }
+            var pos = rawLevel.objs[i][2];
+            obj.phys = new CANNON.Body({
+                mass: mat.physics.mass,
+                position: new CANNON.Vec3(pos[0], pos[1], pos[2]),
+                material: mat.physics,
+                shape: geo.physics
+            });
+            console.log(rawLevel.objs[i][0],geo.physics)
+            var rot = rawLevel.objs[i][3]
+            var tau = Math.PI * 2
+            obj.phys.quaternion.setFromEuler(rot[0] * tau, rot[1] * tau, rot[2] * tau, "XYZ")
+
+            world.addBody(obj.phys);
+
         } catch (e) {
             console.error("Error Loading Object(" + i + ")!");
-            if(geo){ console.error("geo:",geo)}
+            if (geo) { console.error("geo:", geo) }
 
             throw ("Error Loading " + i, e);
 
@@ -81,13 +72,17 @@ module.exports = function (interfaces, rawLevel) {
     }
     level.physicsTick = function (time) {
         //copy physics data to world data
-        world.step();
+        world.step(1 / 60);
 
         for (var i in level.objs) {
             var obj = level.objs[i];
-            //console.log(obj.phys.getPosition())
-            obj.position.copy(obj.phys.getPosition())
-            obj.quaternion.copy(obj.phys.getQuaternion())
+            obj.position.x = obj.phys.position.x
+            obj.position.y = obj.phys.position.y
+            obj.position.z = obj.phys.position.z
+            obj.quaternion.w = obj.phys.quaternion.w
+            obj.quaternion.x = obj.phys.quaternion.x
+            obj.quaternion.y = obj.phys.quaternion.y
+            obj.quaternion.z = obj.phys.quaternion.z
 
         }
         // and copy position and rotation to three mesh
